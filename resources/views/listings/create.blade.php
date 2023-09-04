e6d86033682557849e0ef269577843678d7aca6a
kamran-tahir <kamran.tahir454@gmail.com>
2023-09-04 13:47:36 +0500

    Contain form values after validation error

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -21,7 +21,7 @@
                 <input
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
-                    name="company"
+                    name="company" value="{{ old('company') }}"
                 />
                 @error('company')
                     <div class="text-red-500 text-xs mt-1">
@@ -39,7 +39,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="title"
                     placeholder="Example: Senior Laravel Developer"
-                />
+                    value= "{{ old('title') }}" />
                 @error('title')
                     <div class="text-red-500 text-xs mt-1">
                         {{ $message }}
@@ -57,7 +57,7 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="location"
-                    placeholder="Example: Remote, Boston MA, etc"
+                    placeholder="Example: Remote, Boston MA, etc" value= "{{ old('location') }}"
                 />
                 @error('location')
                     <div class="text-red-500 text-xs mt-1">
@@ -73,7 +73,7 @@
                 <input
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
-                    name="email"
+                    name="email" value= "{{ old('email') }}"
                 />
                 @error('email')
                     <div class="text-red-500 text-xs mt-1">
@@ -92,7 +92,7 @@
                 <input
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
-                    name="website"
+                    name="website" value= "{{ old('website') }}"
                 />
                 @error('website')
                     <div class="text-red-500 text-xs mt-1">
@@ -110,7 +110,8 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="tags"
                     placeholder="Example: Laravel, Backend, Postgres, etc"
-                />
+                    value= "{{ old('tags') }}" />
+
                 @error('tags')
                     <div class="text-red-500 text-xs mt-1">
                         {{ $message }}
@@ -141,7 +142,7 @@
                     name="description"
                     rows="10"
                     placeholder="Include tasks, requirements, salary, etc"
-                ></textarea>
+                    value= "{{ old('description') }}" ></textarea>
                 @error('description')
                     <div class="text-red-500 text-xs mt-1">
                         {{ $message }}
